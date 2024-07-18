45f187b55c850d7dbaeca641fd00242811be74bf
Crazysundar24 <crazysundar16@gmail.com>
2024-07-18 10:55:14 +0530

    Create Hello world.java

---
+++ Hello world.java
@@ -0,0 +1,20 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, E
+World");
+        System.out.println("Hello, World ");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+        System.out.println("Hello, World");
+    }
+}
+
+<!---
+Crazysundar24/Crazysundar24 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
